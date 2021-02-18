1685703fc0616c8d2b1eae0b9a5fb5b349f23ca0
Your Name <you@example.com>
2021-02-18 12:29:12 +0700

    Edit fallback message

--- src/Routes.js
+++ src/Routes.js
@@ -48,7 +48,7 @@ const Routes = () => {
       <Switch>
         <PrivateRoute path="/" exact component={Home} />
         <Route path="/signin" exact component={Signin} />
-        <Route path="/signup" exact component={Signup} />
+        <AdminRoute path="/signup" exact component={Signup} />
         <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
         <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
         <AdminRoute path="/create/category" exact component={AddCategory} />
